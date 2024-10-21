9697608305f67d3f29bc56e96f204f64c1fa66c9
Jonas Gerber <73141623+Traenqui@users.noreply.github.com>
2024-10-21 19:42:27 +0200

    test: added support for nox to run multiple tests
    - for now only python version 3.12 is supported

---
+++ noxfile.py
@@ -0,0 +1,7 @@
+import nox
+
+@nox.session(python=["3.12"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
