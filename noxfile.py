8d53fb046440865e22ff1bf484f84f35892c4945
Jonas Gerber <73141623+Traenqui@users.noreply.github.com>
2024-10-21 20:27:10 +0200

    style: added flake8-import-order
    - set order style to google
    - enable 'I' warning in .flake8
    - added local names to the flake8 config

--- noxfile.py
+++ noxfile.py
@@ -4,10 +4,11 @@ locations = "src", "tests", "noxfile.py"
 python_versions = ["3.12"]
 nox.options.sessions = "lint", "tests"
 
+
 @nox.session(python=python_versions)
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
