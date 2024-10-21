86266d5bf063911efeac007aeb68d09bca3a297f
Jonas Gerber <73141623+Traenqui@users.noreply.github.com>
2024-10-21 20:20:42 +0200

    style: Added support for flake8 and black
    - added seperate command to foramt code with black
    - added default sessions without black session

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # python-template
+
+## manual code formatting
+
+With a `nox` session in place, you can reformat your code like this:
+
+```sh
+nox -rs black
+```
--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,25 @@
 import nox
 
-@nox.session(python=["3.12"])
+locations = "src", "tests", "noxfile.py"
+python_versions = ["3.12"]
+nox.options.sessions = "lint", "tests"
+
+@nox.session(python=python_versions)
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black")
+    session.run("flake8", *args)
+
+
+@nox.session(python=python_versions)
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=python_versions[0])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
